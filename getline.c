949e3f0b0e54cf36ee11cd0e3ec270c5377c3315
Bennyonye <benedictigbukolu@gmail.com>
2023-11-11 13:36:31 +0100

    update-shell

--- getline.c
+++ getline.c
@@ -52,7 +52,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
  */
 ssize_t get_input(info_t *info)
 {
-	static char *buf; /* the ';' command chain buffer */
+	static char *buf;
 	static size_t i, j, len;
 	ssize_t r = 0;
 	char **buf_p = &(info->arg), *p;
--- history.c
+++ history.c
@@ -1,13 +1,13 @@
 #include "shell.h"
 
 /**
- * get_history_file - gets the history file
+ * get_h_file - gets the history file
  * @info: parameter struct
  *
  * Return: allocated string containg history file
  */
 
-char *get_history_file(info_t *info)
+char *get_h_file(info_t *info)
 {
 	char *buf, *dir;
 
@@ -33,7 +33,7 @@ char *get_history_file(info_t *info)
 int write_history(info_t *info)
 {
 	ssize_t fd;
-	char *filename = get_history_file(info);
+	char *filename = get_h_file(info);
 	list_t *node = NULL;
 
 	if (!filename)
@@ -54,17 +54,17 @@ int write_history(info_t *info)
 }
 
 /**
- * read_history - reads history from file
+ * read_hist - reads history from file
  * @info: the parameter struct
  *
  * Return: histcount on success, 0 otherwise
  */
-int read_history(info_t *info)
+int read_hist(info_t *info)
 {
 	int i, last = 0, linecount = 0;
 	ssize_t fd, rdlen, fsize = 0;
 	struct stat st;
-	char *buf = NULL, *filename = get_history_file(info);
+	char *buf = NULL, *filename = get_h_file(info);
 
 	if (!filename)
 		return (0);
@@ -124,12 +124,12 @@ int build_history_list(info_t *info, char *buf, int linecount)
 }
 
 /**
- * reno_history - renumbers the history linked list after changes
+ * renos_history - renumbers the history linked list after changes
  * @info: Structure containing potential arguments. Used to maintain
  *
  * Return: the new histcount
  */
-int reno_history(info_t *info)
+int renos_history(info_t *info)
 {
 	list_t *node = info->history;
 	int i = 0;
