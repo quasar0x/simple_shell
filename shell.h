bcc47bb05f933525ac629f40668b5eae2b0789e9
Benedict Onye <98492400+Bennyonye@users.noreply.github.com>
2023-11-12 16:10:29 +0100

    Update-shell

--- shell.h
+++ shell.h
@@ -216,7 +216,7 @@ int renos_history(info_t *info);
 /* toem_lists.c */
 list_t *add_node(list_t **, const char *, int);
 list_t *add_node_end(list_t **, const char *, int);
-size_t print_list_str(const list_t *);
+size_t print_str(const list_t *);
 int delete_node_at_index(list_t **, unsigned int);
 void free_list(list_t **);
 
